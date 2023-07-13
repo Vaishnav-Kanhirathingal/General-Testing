18ba5939e4cbcc2ef4ebf09833155b94eb10ddd7
Vaishnav Kanhirathingal <vaishnav.kanhira@gmail.com>
2023-07-13 13:53:22 +0530

    implemented Maps Api

--- MapsApi/app/src/main/java/com/example/mapsapi/MainActivity.kt
+++ MapsApi/app/src/main/java/com/example/mapsapi/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.mapsapi
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : OnMapReadyCallback, AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val mapFrag: SupportMapFragment =
+            supportFragmentManager.findFragmentById(R.id.maps_fragment) as SupportMapFragment
+        mapFrag.getMapAsync(this)
+    }
+
+    override fun onMapReady(p0: GoogleMap) {
+        // TODO:
     }
 }
