1c78cf0982b8033b5bf97e4aaf7a9f73853b8b46
Vaishnav-Kanhirathingal <vaishnav.kanhira@gmail.com>
2025-01-03 23:58:59 +0530

    token listener added

--- WorkManager/app/src/main/java/com/example/work_manager/MainActivity.kt
+++ WorkManager/app/src/main/java/com/example/work_manager/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.work_manager
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.messaging.FirebaseMessaging
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = this::class.simpleName
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        FirebaseMessaging.getInstance().token.addOnCompleteListener { task ->
+            Log.d(TAG, if (task.isSuccessful) "FCM Token: ${task.result}" else "task failed")
+        }
     }
 }
