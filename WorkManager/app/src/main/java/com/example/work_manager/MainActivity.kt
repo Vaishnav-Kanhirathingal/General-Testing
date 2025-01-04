eda778e4dad0081758b4e544920e60ed3d873233
Vaishnav-Kanhirathingal <vaishnav.kanhira@gmail.com>
2025-01-04 22:26:48 +0530

    permission request handled

--- WorkManager/app/src/main/java/com/example/work_manager/MainActivity.kt
+++ WorkManager/app/src/main/java/com/example/work_manager/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.example.work_manager
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import com.google.firebase.messaging.FirebaseMessaging
 
 class MainActivity : AppCompatActivity() {
@@ -14,6 +18,18 @@ class MainActivity : AppCompatActivity() {
 
         FirebaseMessaging.getInstance().token.addOnCompleteListener { task ->
             Log.d(TAG, if (task.isSuccessful) "FCM Token: ${task.result}" else "task failed")
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (checkSelfPermission(android.Manifest.permission.POST_NOTIFICATIONS) !=
+                PackageManager.PERMISSION_GRANTED
+            ) {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.POST_NOTIFICATIONS),
+                    101
+                )
+            }
         }
     }
 }
