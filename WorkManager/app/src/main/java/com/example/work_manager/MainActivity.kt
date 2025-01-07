43029a8b9e016e0e26ecce3ce113b5cbd62a07d1
Vaishnav-Kanhirathingal <vaishnav.kanhira@gmail.com>
2025-01-07 17:10:43 +0530

    binding added

--- WorkManager/app/src/main/java/com/example/work_manager/MainActivity.kt
+++ WorkManager/app/src/main/java/com/example/work_manager/MainActivity.kt
@@ -7,14 +7,17 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
+import com.example.work_manager.databinding.ActivityMainBinding
 import com.google.firebase.messaging.FirebaseMessaging
 
 class MainActivity : AppCompatActivity() {
     private val TAG = this::class.simpleName
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         FirebaseMessaging.getInstance().token.addOnCompleteListener { task ->
             Log.d(TAG, if (task.isSuccessful) "FCM Token: ${task.result}" else "task failed")
